032381231accdefbe6fe5f061048f0c88269dfbd
alexjuda <alexanderjuda@gmail.com>
2025-05-03 22:49:32 +0200

    Fix codestyle

--- src/fini/main_argparse.py
+++ src/fini/main_argparse.py
@@ -1,23 +1,25 @@
 import argparse
 
 
-
 def _run_edit():
     print("edit")
+
 
 def _run_rollover():
     print("edit")
 
+
 def _run_sync():
     print("sync")
+
 
 def _parser():
     parser = argparse.ArgumentParser()
 
     subparser = parser.add_subparsers(dest="command")
-    edit_parser = subparser.add_parser("edit")
-    rollover_parser = subparser.add_parser("rollover")
-    sync_parser = subparser.add_parser("sync")
+    _ = subparser.add_parser("edit")
+    _ = subparser.add_parser("rollover")
+    _ = subparser.add_parser("sync")
 
     return parser
 
