aa4617549fd3a9efc8b12d494ccdad1726a557b2
alexjuda <alexanderjuda@gmail.com>
2025-05-03 22:46:41 +0200

    Add sample subcommands

--- src/fini/main_argparse.py
+++ src/fini/main_argparse.py
@@ -1,7 +1,36 @@
 import argparse
 
 
-def app():
+
+def _run_edit():
+    print("edit")
+
+def _run_rollover():
+    print("edit")
+
+def _run_sync():
+    print("sync")
+
+def _parser():
     parser = argparse.ArgumentParser()
-    args = parser.parse_args()
-    print(args)
+
+    subparser = parser.add_subparsers(dest="command")
+    edit_parser = subparser.add_parser("edit")
+    rollover_parser = subparser.add_parser("rollover")
+    sync_parser = subparser.add_parser("sync")
+
+    return parser
+
+
+def app():
+    args = _parser().parse_args()
+
+    match args.command:
+        case "edit":
+            _run_edit()
+
+        case "rollover":
+            _run_rollover()
+
+        case "sync":
+            _run_sync()
