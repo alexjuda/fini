e9a4f85a0556960f5c45a3f270c3ed0bffa84667
Alexander Juda <6004040+alexjuda@users.noreply.github.com>
2025-05-04 23:41:17 +0200

    feat: initial CLI prototype (#1)
    * Adds basic project layout
    * Adds `fini` CLI with `argparse`
    * Adds commands:
        * `fini`
        * `fini edit`
        * `fini rollover`
        * `fini sync`

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from fini!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/fini/main_argparse.py
@@ -0,0 +1,58 @@
+import argparse
+
+
+def _run_edit():
+    from .edit import main
+
+    main()
+
+
+def _run_rollover():
+    from .rollover import main
+
+    main()
+
+
+def _run_sync():
+    from .sync import main
+
+    main()
+
+
+def _run_implicit():
+    print("[rollover]")
+    _run_rollover()
+
+    print("[edit]")
+    _run_edit()
+
+    print("[sync]")
+    _run_sync()
+
+
+def _parser():
+    parser = argparse.ArgumentParser()
+
+    subparser = parser.add_subparsers(dest="command")
+    _ = subparser.add_parser("edit")
+    _ = subparser.add_parser("rollover")
+    _ = subparser.add_parser("sync")
+
+    return parser
+
+
+def app():
+    args = _parser().parse_args()
+
+    match args.command:
+        case "edit":
+            _run_edit()
+
+        case "rollover":
+            _run_rollover()
+
+        case "sync":
+            _run_sync()
+
+        case _:
+            _run_implicit()
