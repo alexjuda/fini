1f45523e970cdc56ad79c47dff44f329d5a8b3e3
alexjuda <alexanderjuda@gmail.com>
2025-05-04 23:38:34 +0200

    Make edit log nicer

--- src/fini/edit/_edit.py
+++ src/fini/edit/_edit.py
@@ -5,7 +5,7 @@ from .._files import today_todo_path
 
 def main():
     cmd = [editor(), str(today_todo_path())]
-    print(f"Launching {cmd}")
+    print(f"Launching $({' '.join(cmd)})")
     proc = subprocess.run(cmd)
     if proc.returncode != 0:
         raise ValueError(f"Running '{cmd}' failed with code {proc.returncode}")
