b2f3f7a31670f05f62b4d080a8836b39f2feb3b1
alexjuda <alexanderjuda@gmail.com>
2025-05-04 21:00:04 +0200

    Parse file dates

--- src/fini/_files.py
+++ src/fini/_files.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from datetime import date, datetime, timezone
 from pathlib import Path
 
@@ -15,5 +16,39 @@ def todo_file() -> Path:
     return (dir / today.isoformat()).with_suffix(".md")
 
 
-def all_todo_files() -> list[Path]:
-    pass
+# def all_todo_files() -> list[Path]:
+#     ...
+
+
+# def _
+
+
+@dataclass
+class TodoFile:
+    path: Path
+    date_: date
+
+    @classmethod
+    def parse_path(cls, path: Path) -> "TodoFile | None":
+        try:
+            date_ = date.fromisoformat(path.stem)
+        except ValueError:
+            return None
+
+        return cls(path=path, date_=date_)
+
+
+
+def prev_day_todo_file() -> TodoFile | None:
+    dir = fini_dir()
+    todo_files = (
+        file
+        for path in dir.iterdir()
+        if (file := TodoFile.parse_path(path))
+    )
+
+    today = _today_date()
+    prev_files = (file for file in todo_files if file.date_ != today)
+    yesterday_file = max(prev_files, key=lambda f: f.date_)
+
+    return yesterday_file
--- src/fini/_files_test.py
+++ src/fini/_files_test.py
@@ -1,9 +1,13 @@
+from pathlib import Path
 import pytest
 import re
-from ._files import todo_file
+from datetime import date
+from freezegun import freeze_time
+from ._files import TodoFile, todo_file, prev_day_todo_file
 
 
-class TestTodoFile:
+# TODO: rename that function
+class TestTodoFileFn:
     @staticmethod
     def test_path_looks_legit(monkeypatch: pytest.MonkeyPatch):
         monkeypatch.setenv("FINI_DIR", "/home/alex/Documents/todos")
@@ -17,3 +21,74 @@ class TestTodoFile:
             )
             is not None
         )
+
+
+DAY_BEFORE_YESTERDAY = date.fromisoformat("2025-02-01")
+YESTERDAY = date.fromisoformat("2025-02-02")
+TODAY = date.fromisoformat("2025-02-03")
+
+
+class TestPrevDateTodoFile:
+    @staticmethod
+    @pytest.fixture
+    def fini_dir(monkeypatch: pytest.MonkeyPatch, tmp_path: Path) -> Path:
+        dir = tmp_path / "fini_dir"
+        dir.mkdir()
+        monkeypatch.setenv("FINI_DIR", str(dir))
+        return dir
+
+    @staticmethod
+    def test_no_files():
+        ...
+
+    @staticmethod
+    def test_today_only():
+        ...
+
+    @staticmethod
+    def test_no_today():
+        ...
+
+    @staticmethod
+    @freeze_time(TODAY)
+    def test_multiple_files(fini_dir: Path):
+        for day in [TODAY, YESTERDAY, DAY_BEFORE_YESTERDAY]:
+            (fini_dir / day.isoformat()).with_suffix(".md").touch()
+
+        file = prev_day_todo_file()
+
+        assert file is not None
+        assert file.date_ == YESTERDAY
+
+    @staticmethod
+    def test_not_matching_filenames():
+        ...
+
+
+class TestTodoFile:
+    class TestParsePath:
+        @staticmethod
+        def test_valid():
+            path = Path("notes/2025-01-02.md")
+
+            file = TodoFile.parse_path(path)
+
+            assert file is not None
+            assert file.path.suffix == ".md"
+            assert file.date_ == date(2025, 1, 2)
+
+        @staticmethod
+        def test_no_date():
+            path = Path("notes/foo.md")
+
+            file = TodoFile.parse_path(path)
+
+            assert file is None
+
+        @staticmethod
+        def test_date_with_things():
+            path = Path("notes/foo-2025-03-04-bar.md")
+
+            file = TodoFile.parse_path(path)
+
+            assert file is None
