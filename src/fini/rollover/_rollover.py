f2ec60b468149fc65c86eff356b8adada40166ec
alexjuda <alexanderjuda@gmail.com>
2025-05-23 23:00:21 +0200

    refactor: extract pattern regex

--- src/fini/rollover/_rollover.py
+++ src/fini/rollover/_rollover.py
@@ -4,10 +4,14 @@ from pathlib import Path
 from .._files import prev_day_todo, today_todo_path
 
 
+DONE_TASK = re.compile(r"^\s*[-\*] +\[x]")
+
+
 def rollover_file(prev_path: Path, new_path: Path):
     with prev_path.open() as f_in, new_path.open("w") as f_out:
         for line in f_in:
-            if not re.match(r"^\s*[-\*] +\[x]", line):
+            match = DONE_TASK.match(line)
+            if not match:
                 f_out.write(line)
 
 
