d4f978d0b1a23960da3f623aa7eaec04fee6b45b
Alexander Juda <6004040+alexjuda@users.noreply.github.com>
2025-05-24 09:51:46 +0200

    feat: drop finished items during rollover (#5)
    Fixes #4

--- src/fini/rollover/_rollover.py
+++ src/fini/rollover/_rollover.py
@@ -1,5 +1,56 @@
-import shutil
-from .._files import today_todo_path, prev_day_todo
+import re
+from pathlib import Path
+
+from .._files import prev_day_todo, today_todo_path
+
+
+DONE_TASK_PATTERN = re.compile(r"^\s*([-\*]) +\[x]")
+INDENT_PATTERN = re.compile(r"^\s*(\S)")
+
+
+def line_indent(line: str) -> int:
+    match = INDENT_PATTERN.match(line)
+    if not match:
+        return 0
+    return match.start(1)
+
+
+def _drop_done(prev_path: Path, new_path: Path):
+    with prev_path.open() as f_in, new_path.open("w") as f_out:
+        done_section_indent = None
+        for line in f_in:
+            current_indent = line_indent(line)
+
+            if done_section_indent is not None:
+                if done_section_indent < current_indent:
+                    continue
+                else:
+                    # We're past the section.
+                    done_section_indent = None
+
+            match = DONE_TASK_PATTERN.match(line)
+            # Find out the indentation of the section we want to remove.
+            if (
+                # When the first line matches '* [x]'
+                match
+                # When we aren't already in a nested section.
+                and not done_section_indent
+            ):
+                done_section_indent = match.start(1)
+
+            if not match:
+                f_out.write(line)
+
+
+def _drop_multi_empty_lines(path: Path):
+    text = path.read_text()
+    fixed_text = re.sub(r"\n\n+", "\n\n", text)
+    path.write_text(fixed_text)
+
+
+def rollover_file(prev_path: Path, new_path: Path):
+    _drop_done(prev_path, new_path)
+    _drop_multi_empty_lines(new_path)
 
 
 def main():
@@ -12,5 +63,5 @@ def main():
     if not (prev_todo := prev_day_todo()):
         raise ValueError("No prev day todo file found")
 
-    shutil.copy(prev_todo.path, todo_path)
-    print(f"Copied {prev_todo.path.name} to {todo_path.name}")
+    rollover_file(prev_todo.path, todo_path)
+    print(f"Rolled over {prev_todo.path.name} to {todo_path.name}")
